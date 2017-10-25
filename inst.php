f62fb5b5e97458f763c2315b1ab78ffa0f9ed960
Jacob-kievit <kievitboy@att.net>
2017-10-25 16:05:35 -0500

    upto date files
    this will work as is in xampp. Use mike for username and mike for password

---
+++ inst.php
@@ -0,0 +1,96 @@
+<?php
+
+SESSION_start();
+
+require_once("page.php");
+require_once("formClass.php");
+
+if (isset($_SESSION['istrue'])) {
+$page = new Page();
+$form = new Form();
+//$page->addHeadItem("<script type='text/javascript' src=hello.js></script>");
+$page->setHeadSection("<link rel='stylesheet' type='text/css' href='pretty.css'>");
+$page->setTop();
+$form->setForm();
+$page->setBottom();
+
+
+
+
+$page->getTop();
+$form->getForm();
+$page->getBottom();
+
+
+	
+
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		//$fh = @fopen("/home/jkiev461/webfiles/music.txt","a+"); // file path for school computers
+		$fh = @fopen("music.txt","a+"); // file path for xammpp
+		
+		//checking if $fh exists 
+		if (is_resource($fh)) 
+		{
+				//   
+				if (isset($_POST['submit'])){
+					
+					// grabs the variables
+					$date = time();
+					$announcer = $_SESSION['username'];
+					$songtitle = $_POST['songTitle'];
+					$songartist = $_POST['songArtist'];
+					$album = $_POST['album'];
+					$label = $_POST['label'];
+					$list = array( $date, $announcer, $songtitle, $songartist, $album, $label ); // need array to make the delimiter work    			
+					
+					
+					fputcsv($fh, $list); // writes file in delimited format
+					fclose($fh); // close file
+					
+					//$file = @fopen("/home/jkiev461/webfiles/music.txt","r"); // file path for school computers
+					$file = @fopen("music.txt","r"); // file path for xammpp
+					if (is_resource($file)) {
+						
+						while ($line = fgets($file)) {
+							
+							$timeStamp = explode(",",$line);
+							$time = $timeStamp[0];
+							$angst = 3600; //hour in seconds ANGST ANGST ANGST ANGST
+							if($time > (time() - $angst))
+							{
+								print nl2br($line);// nl2br — Inserts HTML line breaks before all newlines in a string
+							}
+								
+						} // end while
+						fclose($file);
+					} else {
+						print "unable to open file";
+					}// end if is_resource
+					
+					// display file
+					//file path for school computers("/home/jkiev461/webfiles/music.txt");
+					//$file = ("/home/jkiev461/webfiles/music.txt");
+					//$file = "music.txt"; //file path
+					
+					//$screentext = file_get_contents($file); //file_get_contents — Reads entire file into a string
+					//$screentext = nl2br($screentext); // nl2br — Inserts HTML line breaks before all newlines in a string
+					//echo $screentext; // pukes the file out on the screen all nice and formated 
+				}
+
+		}// end if 
+	}
+} else{
+	print "<h1>you are not logged in</h1>";
+}	//end session if statement
+
+
+?>
+
+
+
+
+
+
+
---
+++ login.php
@@ -0,0 +1,57 @@
+<?php
+SESSION_start();
+require_once "page.php";
+$page = new Page("Form");
+$page->setHeadSection("<link rel='stylesheet' type='text/css' href='pretty.css'>");
+$page->setTop();
+$page->setBottom();
+$errors = array();
+$loggedIn = false;
+print $page->getTop();
+//improve this logic! it is terrible
+if (isset($_POST['username']))
+{
+		$fh = @fopen("password.txt","r");// xammp file path
+		//$fh = @fopen("/home/jkiev461/webfiles/password.txt","r"); // school file path
+		if (is_resource($fh))
+		{
+        while ($line = fgets($fh)) 
+				{
+						$creds = explode("|",$line);
+						if ($_POST['username'] == rtrim($creds[0]) && 
+									$_POST['password'] == rtrim($creds[1]))
+						{
+								$loggedIn = true;
+								$_SESSION['istrue'] = 'its set';
+						}  					
+				} //end while
+		}	
+		else
+		{
+			print "unable to open file";
+		}
+		
+	if (!$loggedIn) {
+		$errors[] = "Username or password incorrect";
+	}
+	else 
+	{
+		print "Welcome";
+		$_SESSION['istrue'] = 'its set';
+		$_SESSION['username'] = $_POST['username'];
+		header('Location: inst.php');/// redirect to new page
+	}
+}
+if (count($errors) > 0 || !isset($_POST['username'])) 
+{
+  foreach ($errors as $error) {
+    print $error . "<br />\n";
+  }
+  print "<form action='login.php' method='POST'>\n";
+  print "Username: <input type='text' name='username'><br />\n";
+  print "Password: <input type='password' name='password'><br />\n";
+  print "<input type='submit' name='submit'>\n";
+  print "</form>\n";
+}
+print $page->getBottom();
+?>
