310b49b78b42a344a78938d5b2b4be19b83b5648
Jacob-kievit <kievitboy@att.net>
2017-10-29 18:09:13 -0500

    update formClass.php
    fixed it so html code will be valid

--- formClass.php
+++ formClass.php
@@ -14,7 +14,7 @@ class Form extends Page {
 		
 		$this->_form .= "";
 		$this->_form .= "<form method='post'>";
-		$this->_form .= "<label>Stack </label><select><option value='op1'>Option 1</option><option value='op2'>Option 2</option><option value='op3'>Option 3</option><option  value='op4'>Option 4</option></select></br>";
+		$this->_form .= "<label>Stack </label><select><option value='op1'>Option 1</option><option value='op2'>Option 2</option><option value='op3'>Option 3</option><option  value='op4'>Option 4</option></select><br>";
 		$this->_form .= "<label>Song Title </label><input type='text' name='songTitle' required><br>";
 		$this->_form .= "<label>Song Artist </label><input type='text' name='songArtist' required><br>";
 		$this->_form .= "<label>Album </label><input type='text' name='album' required><br>";
