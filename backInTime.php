26f2ac66c75d8e2ecf16e1abefb48de4d90636e7
Jacob-kievit <kievitboy@att.net>
2017-10-30 08:07:53 -0500

    some changes

--- backInTime.php
+++ backInTime.php
@@ -17,7 +17,7 @@
 */
 
 SESSION_start();
-
+ 
 require_once("page.php");
 require_once("formClass.php");
 
@@ -27,8 +27,9 @@ if (isset($_SESSION['istrue'])) {
 	//$page->addHeadItem("<script type='text/javascript' src=hello.js></script>");
 	$page->setHeadSection("<link rel='stylesheet' type='text/css' href='pretty.css'>");
 
+	
 	$page->setTop();
-	$page->setBottomSection("<a href='backInTime.php'>Want to go back more??</a><br>");
+	$page->setBottomSection("<a href='backInTime.php'>  Want to go back more??</a><br>");
 	//$form->setForm();
 	$page->setBottom();
 
@@ -36,26 +37,37 @@ if (isset($_SESSION['istrue'])) {
 	//$form->getForm();
 	$page->getBottom();
 
-		//$fh = @fopen("/home/jkiev461/webfiles/music.txt","r"); // file path for school computers
-		$file = @fopen("music.txt","r"); // file path for xammpp
+		$file = @fopen("/home/jkiev461/webfiles/music.txt","r"); // file path for school computers
+		//$file = @fopen("music.txt","r"); // file path for xammpp
 		
 	if (is_resource($file)) {
-						
+			
+		$angst = 3600; // 3600 seconds in an hour ANGST ANGST ANGST ANGST
+		$hell = 7200;				
+		
 		while ($line = fgets($file)) {
 							
 			$timeStamp = explode("|",$line);
 			$time = $timeStamp[0];
-			$angst = 3600; // 3600 seconds in an hour ANGST ANGST ANGST ANGST
-		
-			if($time > (time() - $angst)) {
+			 
+			
+			//$angst = 3600; // 3600 seconds in an hour ANGST ANGST ANGST ANGST
+			//$hell = 7200; 
+			if($time < (time() - $angst) && $time > (time() - $hell))  {
 					
 				date_default_timezone_set('America/Chicago');// sets timezone to the America/Chicago timezone 
 				$lineParts = explode("|", $line);
 				$derp = (date('m/d h:iA ',$lineParts[0])) . ' ' . $lineParts[1] . ' ' . $lineParts[2] . ' ' . $lineParts[3] . ' ' . $lineParts[4] . ' ' . $lineParts[5];
 				print nl2br($derp);// nl2br — Inserts HTML line breaks before all newlines in a string
-								
-			}/// hypr link with a query string to go back to prevous hours 
-							
+				
+
+			} else {
+				print ' test fail ';
+			} 
+			//$angst += 3600; 
+			//$hell += 3600;
+
+			
 		} // end while
 			fclose($file);
 	} else {
