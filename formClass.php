164827032c5b17dafef38f12c63b6a6a3ebdb050
Jacob-kievit <kievitboy@att.net>
2017-10-29 19:09:29 -0500

    added code to add to form

--- formClass.php
+++ formClass.php
@@ -5,7 +5,7 @@ require_once("page.php");
 class Form extends Page {
 	
 	private $_form;
-	
+	private $_formSection = "";
 	function getForm() {
 		print $this->_form;
 	}
@@ -20,8 +20,10 @@ class Form extends Page {
 		$this->_form .= "<label>Album </label><input type='text' name='album' required><br>";
 		$this->_form .= "<label>Label </label><input type='text' name='label' required><br>";
 		$this->_form .= "<input type='submit' name='submit'></form>";
+		$this->_form .= $this->_formSection;
 	}
-	
-	
+	function setformSection($include) {
+	  $this->_formSection .= $include;
+	} //end function setBottomSection
 }
 ?>
