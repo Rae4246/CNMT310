ef4160d8a6f74341aa86979fb210c9163f41c1d1
sclavinator <32905144+sclavinator@users.noreply.github.com>
2017-11-13 09:49:27 -0600

    changes

--- Project Tables.sql
+++ Project Tables.sql
@@ -1,61 +1,66 @@
 CREATE TABLE User (
 	user_id int not null primary key auto_increment,
-	username varchar(255),
-	password varchar(255),
-	status varchar(255),
-	last_login datetime,
-	name varchar(255)
+    	username varchar(255),
+    	pass varchar(255),
+    	stat varchar(255),
+    	last_login datetime,
+    	real_name varchar(255)
 );
+
 
 CREATE TABLE UserRole (
 	user_role_id int not null primary key auto_increment,
-   	user_id int,
-	role_id int,
-	last_change datetime
+    	user_id int,
+    	role_id int,
+    	last_change datetime
 );
 
 CREATE TABLE Role (
 	role_id int not null primary key auto_increment,
-	role_name varchar(255)
+    	role_name varchar(255)
 );
 
 CREATE TABLE MusicEntries (
 	musicEntryID int not null primary key auto_increment,
 	song_id int,
-	album_id int,
-	label_id int,
-	stack_id int,
-	artist_id int,
-	timestamp datetime
+    	album_id int,
+    	label_id int,
+    	stack_id int,
+    	artist_id int,
+    	time_stamp datetime
 );
 
 CREATE TABLE Songs (
 	song_id int not null primary key auto_increment,
-	song_name varchar(255),
-	album_id int,
-	label_id int,
-	stack_id int,
-	artist_id int
+    	song_name varchar(255),
+    	album_id int not null,
+    	label_id int,
+    	stack_id int,
+    	artist_id int
 );
+
 
 CREATE TABLE Album (
 	album_id int not null primary key auto_increment,
-	album_name varchar(255),
-	artist_id int,
-	label_id int
+    	album_name varchar(255),
+    	artist_id int,
+    	label_id int
 );
 
 CREATE TABLE Artist (
 	artist_id int not null primary key auto_increment,
-	album_name varchar(255)
+    	album_name varchar(255)
 );
 
 CREATE TABLE Label (
 	label_id int not null primary key auto_increment,
-	label_name varchar(255)
+    	label_name varchar(255)
 );
 
 CREATE TABLE Stack (
 	stack_id int not null primary key auto_increment,
-	stack_name int
+    	stack_name int
 );
+
+ALTER TABLE userrole
+drop foreign key user_id;
