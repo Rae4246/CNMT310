ab583a40668d274cf61073ffc82720b86e973a12
Jacob-kievit <kievitboy@att.net>
2017-11-20 07:05:05 -0600

    have the log ing and some logic for the database figured out

--- inst.php
+++ inst.php
@@ -7,33 +7,64 @@ SESSION_start();
 
 require_once("page.php");
 require_once("formClass.php");
-
-if (isset($_SESSION['istrue'])) {
+require_once("DB.class.php");
+require_once("login.Class.php");
 $page = new Page();
 $form = new Form();
-//$page->addHeadItem("<script type='text/javascript' src=hello.js></script>");
+$login = new LoginForm();
+
+$login->setLoginForm();
+
+
 $page->setHeadSection("<link rel='stylesheet' type='text/css' href='style_log.css'>");
 $page->setHeadSection("<link rel='stylesheet' type='text/css' href='style.css'>");
 $page->setHeadSection("<link rel='stylesheet' type='text/css' href='style_acc.css'>");
 $page->setHeadSection("<link rel='stylesheet' type='text/css' href='style_prev.css'>");
-$page->setNavSection("<ul><li><a href='#'>Previously Played</a></li><li><a href='#'>Playlist Log</a></li><li><a href='#'>Free-Form Reporting</a></li><li><a href='#'>Admin Edit Page</a></li></ul>");
+$page->setNavSection("<ul><li><a href='backInTime.php'>Previously Played</a></li><li><a href='#'>Playlist Log</a></li><li><a href='#'>Free-Form Reporting</a></li><li><a href='#'>Admin Edit Page</a></li></ul>");
 
 $page->setHeaderCoverSection("<div class='head-title'><h1>WWSP - 90FM<h1><h2>DJ Hub</h2></div>");
 $page->setHeaderCoverSection("<div class='head-logo'><img src='WWSP_90fm_mic.png'></div>");
-$page->setHeaderCoverSection("<div class='head-login'>Login Form Goes here</div>");
+
+
+$page->setHeaderCoverSection("<div class='head-login'>");
+$page->setHeaderCoverSection($login->getLoginForm());
+$page->setHeaderCoverSection("</div>");							  
+
+							  
 $page->setTop();
+
+// adding music entrys 
+if (isset($_SESSION['istrue'])) {
+
 $form->setformSection("<a href='backInTime.php'>Want to go back more??</a>");
-
 $form->setForm();
-$page->setBottom();
-
-$page->getTop();
-$form->getForm();
-$page->getBottom();
-
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		
+		// NEED TO ESTABLISH DATBASE CONNECTION
+		if($status = true){
+			dbConnect();
+			if (isset($_POST['submit'])){
+					
+					// grabs the variables
+					$date = time();
+					$announcer = $_SESSION['username'];
+					$songtitle = $_POST['songTitle'];
+					$songartist = $_POST['songArtist'];
+					$album = $_POST['album'];
+					$label = $_POST['label'];
+					//$list = array( $date, $announcer, $songtitle, $songartist, $album, $label ); // need array to make the delimiter work    			
+					$query = "INSERT INTO [ TABLE NAME] (STUFF, STUFF ,STUFFF ,STUFF)
+													VALUES (STUFF1, STUFF2, STUFF3, STUFF4)";
+			}										
+		} else{
+			print "can not connect to server";
+		}
+	} 
+} else{
+	print "<h1>you are not logged in</h1><br>";
+	print "you need to log in<br>";
+}	//end session if statement
+/*		
 		//$fh = @fopen("../webfiles/music.txt","a+"); // file path for school computers
 		$fh = @fopen("music.txt","a+"); // file path for xammpp
 		
@@ -79,16 +110,18 @@ $page->getBottom();
 					} else {
 						print "unable to open file";
 					}// end if is_resource
-					
+					8
 				}
 
-		}// end if 
-	}
-} else{
-	print "<h1>you are not logged in</h1><br>";
-	print "<a href='login.php'> you need to log in</a><br>";
-}	//end session if statement
+		}// end if resource 
+		*/
+	
 
+
+$page->setBottom();
+$page->getTop();
+$form->getForm();
+$page->getBottom();
 
 ?>
 
